bc72a70d8b92d3558ef3fa222827a84c3c967d4e
zhavrid <zhavridanna7@gmail.com>
2023-11-13 13:47:40 +0300

    Добавила ссылку плагина в навигацию

--- lib.php
+++ lib.php
@@ -38,3 +38,15 @@ function local_greetings_get_greeting($user) {
     }
     return get_string($langstr, 'local_greetings', fullname($user));
 }
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+    get_string('pluginname', 'local_greetings'),
+    new moodle_url('/local/greetings/index.php'),
+    navigation_node::TYPE_CUSTOM, );
+}
