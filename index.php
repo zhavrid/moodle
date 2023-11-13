d50d916addbcd5080329f1dcb84bca128c8d18fe
zhavrid <zhavridanna7@gmail.com>
2023-11-13 16:36:11 +0300

    Вывод текста из бд

--- index.php
+++ index.php
@@ -32,6 +32,17 @@ $PAGE->set_pagelayout('standard');
 $PAGE->set_title($SITE->fullname); // ...Название вместо URL.
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 $messageform = new \local_greetings\form\message_form();
+if ($data = $messageform->get_data()) {
+    $message = required_param('message', PARAM_TEXT);
+
+    if (!empty($message)) {
+        $record = new stdClass;
+        $record->message = $message;
+        $record->timecreated = time();
+
+        $DB->insert_record('local_greetings_messages', $record);
+    }
+}
 
 echo $OUTPUT->header();
 
@@ -42,9 +53,20 @@ if (isloggedin()) {
 }
 
 $messageform->display();
-if ($data = $messageform->get_data()) {
-    $message = required_param('message', PARAM_TEXT);
-    echo $OUTPUT->heading($message, 4);
+$messages = $DB->get_records('local_greetings_messages');
+echo $OUTPUT->box_start('card-columns');
+
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
 }
+
+echo $OUTPUT->box_end();
 
 echo $OUTPUT->footer();
